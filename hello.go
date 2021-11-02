fee5cf40f2c3916207b002ca2e6ef667515246d5
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2021-11-02 16:03:55 +0700

    Update files

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"rsc.io/quote"
+)
 
 func main() {
-	fmt.Println(("Hello, World!"))
+	fmt.Println(quote.Go())
+	fmt.Println("Welcome to the playground!")
+	fmt.Println("The time is", time.Now())
 }
